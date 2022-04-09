fa1a781734d0ed95279d54cd2b5d28070db3f2c6
Kid Sysco <>
2022-04-09 17:15:20 -0500

    meet the team, and roadmap

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -30,7 +30,7 @@
       </v-row>
       <v-row>
         <v-col cols="12" md="4">
-          <v-card class="mx-auto">
+          <v-card class="rounded-t-xl">
             <v-img
               class="white--text align-end"
               height="200px"
@@ -44,35 +44,47 @@
               <div>Jacksonville, FL</div>
             </v-card-text>
             <v-card-actions>
-              <v-btn color="orange" text> Share </v-btn>
-              <v-btn color="orange" text> Explore </v-btn>
+              <v-btn
+                color="orange"
+                text
+                href="https://www.nftystays.com/"
+                target="_new"
+              >
+                <v-icon class="mr-2">mdi-home-group</v-icon>
+                Explore
+              </v-btn>
             </v-card-actions>
           </v-card>
         </v-col>
         <v-col cols="12" md="4">
-          <v-card class="mx-auto">
+          <v-card class="rounded-t-xl">
             <v-img
               class="white--text align-end"
               height="200px"
-              src="/img/gc-type-ghastly.jpg"
+              src="/img/gc-type-fiendish.jpg"
             >
             </v-img>
             <v-card-title>Gorecats</v-card-title>
             <v-card-subtitle class="pb-0">
-              Dig-A-Hash Customer
+              Dig-A-Hash Business Customer
             </v-card-subtitle>
             <v-card-text class="text--primary">
               <div>Cartoon/Comic Book NFT on Solana</div>
               <div>Web2, Web3, NFT Soft-Staking, and NFT Games</div>
             </v-card-text>
             <v-card-actions>
-              <v-btn color="orange" text> Share </v-btn>
-              <v-btn color="orange" text> Explore </v-btn>
+              <v-btn
+                color="orange"
+                text
+                href="https://www.gorecats.io/"
+                target="_new"
+                ><v-icon class="mr-2">mdi-knife</v-icon> Explore
+              </v-btn>
             </v-card-actions>
           </v-card>
         </v-col>
         <v-col cols="12" md="4">
-          <v-card class="mx-auto">
+          <v-card class="rounded-t-xl">
             <v-img
               class="white--text align-end"
               height="200px"
@@ -81,16 +93,296 @@
             </v-img>
             <v-card-title>Darkside Metaverse Tattoos</v-card-title>
             <v-card-subtitle class="pb-0">
-              Dig-A-Hash Customer
+              Dig-A-Hash Business Customer
             </v-card-subtitle>
             <v-card-text class="text--primary">
               <div>Tattoo Art for Metaverse Avatars</div>
               <div>NFT collections created by real tattoo artists.</div>
             </v-card-text>
             <v-card-actions>
-              <v-btn color="orange" text> Share </v-btn>
-              <v-btn color="orange" text> Explore </v-btn>
+              <v-btn
+                color="orange"
+                text
+                href="https://exchange.art/collections/Darkside%20Tattoo%20Collection/nfts"
+                target="_new"
+              >
+                <v-icon class="mr-2">mdi-skull</v-icon> Explore
+              </v-btn>
             </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-card class="rounded-t-xl">
+            <v-img
+              class="white--text align-end"
+              height="200px"
+              src="/img/CryptoRichDeluxeAlbumCover-small.jpg"
+            >
+            </v-img
+            ><v-card-title>DYL NFT</v-card-title>
+            <v-card-subtitle class="pb-0"> Dig-A-Hash Partner </v-card-subtitle>
+            <v-card-text class="text--primary">
+              <div>Music NFTs, Audio/Video Production</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                color="orange"
+                text
+                href="https://www.itslit.org/crypto-rich-deluxe"
+                target="_new"
+              >
+                <v-icon class="mr-2">mdi-music</v-icon> Explore
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col
+          cols="12"
+          md="4"
+          class="d-none d-md-flex align-center justify-center text-center blue--text indigo--text text--darken-3"
+          >Coming <br />
+          Soon!</v-col
+        >
+        <v-col
+          cols="12"
+          md="4"
+          class="d-none d-md-flex align-center justify-center text-center blue--text indigo--text text--darken-3"
+          >Coming <br />
+          Soon!</v-col
+        >
+      </v-row>
+      <v-row>
+        <v-col
+          cols="12"
+          class="mt-16 text-center orange--text text--lighten-1 px-12"
+          ><h2>Metaverse Art Galleries</h2></v-col
+        >
+      </v-row>
+      <v-row dense>
+        <v-col cols="12" md="3"></v-col>
+        <v-col cols="12" md="6" class="px-12"
+          >Dig-A-Hash Holdings shares our vaults of fine art with the world
+          through our Metaverse Art Galleries. Explore our rotating art
+          collections in 3D, all you need is a web browser. So step in to the
+          metaverse with us, and have some fun!</v-col
+        >
+        <v-col cols="12" md="3"></v-col>
+      </v-row>
+      <v-row class="mt-16" dense>
+        <v-col cols="12">
+          <h2 class="light-green--text text--accent-4">Meta Galleries</h2>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-card class="rounded-t-xl">
+            <v-img
+              class="white--text align-end"
+              height="350px"
+              src="/img/planet-hologram.jpg"
+            >
+            </v-img
+            ><v-card-title>Planet Hologram-One</v-card-title>
+            <v-card-subtitle class="pb-0">
+              Dig-A-Hash NFT Meta Gallery
+            </v-card-subtitle>
+            <v-card-text class="text--primary">
+              <div>
+                Explore the gallery in 3D below, or view the 2D Matrica Gallery.
+              </div>
+            </v-card-text>
+            <v-card-actions class="d-flex flex-wrap">
+              <v-btn
+                color="orange"
+                text
+                href="https://www.desolate.space/planet/4993"
+                target="_new"
+              >
+                Explore <v-icon class="mx-2">mdi-rotate-3d</v-icon> Gallery
+              </v-btn>
+              <v-btn
+                color="orange"
+                text
+                href="https://map.desolate.space/?planet=4755"
+                target="_new"
+              >
+                <v-icon class="mr-2">mdi-space-station</v-icon> Planetary Map
+              </v-btn>
+              <v-btn
+                color="orange"
+                href="https://matrica.io/hologramOne"
+                target="_new"
+                text
+              >
+                <v-icon class="mr-2">mdi-palette</v-icon> Matrica
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          cols="12"
+          class="mt-16 text-center orange--text text--lighten-1 px-12"
+          ><h2>Roadmap</h2></v-col
+        >
+      </v-row>
+      <v-row dense class="mb-4">
+        <v-col cols="12" md="3"></v-col>
+        <v-col cols="12" md="6" class="px-12"
+          >The roadmap is constantly evolving, <br class="d-none sm-block" />so
+          we update it frequently. Stay tuned for more soon!</v-col
+        >
+        <v-col cols="12" md="3"></v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-timeline align-top :dense="$vuetify.breakpoint.smAndDown">
+            <v-timeline-item color="light-blue accent-4">
+              <h3 class="mb-4">Phase One</h3>
+              <div>
+                <ul>
+                  <li>Launch Socials</li>
+                  <li>Launch Website</li>
+                </ul>
+              </div>
+            </v-timeline-item>
+            <v-timeline-item color="green accent-4">
+              <div class="d-flex justify-md-end">
+                <div>
+                  <h3 class="mb-4 text-right">Phase Two</h3>
+                  <ul>
+                    <li>Build NFTy Stay Brand</li>
+                    <li>Establish Collaborations</li>
+                    <li>1st Monetization Project</li>
+                  </ul>
+                </div>
+              </div>
+            </v-timeline-item>
+            <v-timeline-item color="amber darken-3">
+              <h3 class="mb-4">Phase Three</h3>
+              <div>
+                <ul>
+                  <li>Back 1st NFT Project Launch</li>
+                  <li>Build Out Infrastructure</li>
+                </ul>
+              </div>
+            </v-timeline-item>
+            <v-timeline-item color="purple accent-3">
+              <div class="d-flex justify-md-end">
+                <div>
+                  <h3 class="mb-4 text-right">Phase Four</h3>
+                  <ul>
+                    <li>Update Roadmap to v 3.0</li>
+                  </ul>
+                </div>
+              </div>
+            </v-timeline-item>
+          </v-timeline>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          cols="12"
+          class="mt-16 text-center orange--text text--lighten-1 px-12"
+          ><h2>Meet the Team</h2></v-col
+        >
+      </v-row>
+      <v-row dense>
+        <v-col cols="12" md="3"></v-col>
+        <v-col cols="12" md="6" class="px-12"
+          >The teams has a wide breadth of experience in business, branding,
+          software development, NFTs, and Crypto Currency.</v-col
+        >
+        <v-col cols="12" md="3"></v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-card class="rounded-t-xl mx-auto" max-width="340">
+            <div class="text-center">
+              <v-avatar color="primary" size="256" class="mt-6">
+                <v-img height="300px" src="/img/avatar-recon.jpg"> </v-img
+              ></v-avatar>
+            </div>
+            <v-card-title>Recon Whispers</v-card-title>
+            <v-card-subtitle class="pb-0">
+              Dig-A-Hash Founder & CEO
+            </v-card-subtitle>
+            <v-card-text class="text--primary">
+              <div>
+                <ul>
+                  <li>Business Builder</li>
+                  <li>Strategic Mastermind</li>
+                  <li>Serial Entrepreneur</li>
+                </ul>
+              </div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                color="orange"
+                text
+                href="https://twitter.com/reconwhispers"
+                target="_new"
+              >
+                <v-icon class="mr-2">mdi-twitter</v-icon>
+                Twitter
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-card class="rounded-t-xl mx-auto" max-width="340">
+            <div class="text-center">
+              <v-avatar color="primary" size="256" class="mt-6">
+                <v-img height="300px" src="/img/avatar-kid.jpg"> </v-img
+              ></v-avatar>
+            </div>
+            <v-card-title>Hologram, The Dig-A-Hash Kid</v-card-title>
+            <v-card-subtitle class="pb-0">
+              FinTech Software Developer
+            </v-card-subtitle>
+            <v-card-text class="text--primary">
+              <div>
+                25 Years in ...
+                <ul>
+                  <li>JavaScript UI</li>
+                  <li>PhotoShop</li>
+                </ul>
+              </div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                color="orange"
+                text
+                href="https://twitter.com/Dig_A_Hash_Kid"
+                target="_new"
+              >
+                <v-icon class="mr-2">mdi-twitter</v-icon>
+                Twitter
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-card class="rounded-t-xl mx-auto" max-width="340">
+            <div class="text-center">
+              <v-avatar color="primary" size="256" class="mt-6">
+                <v-img height="300px" src="/img/avatar-boris.png"> </v-img
+              ></v-avatar>
+            </div>
+            <v-card-title>Boristotle</v-card-title>
+            <v-card-subtitle class="pb-0"> Software Engineer </v-card-subtitle>
+            <v-card-text class="text--primary">
+              <div>
+                <ul>
+                  <li>Real Estate Investor</li>
+                  <li>Cinnamon bun enthusiast</li>
+                </ul>
+              </div>
+            </v-card-text>
+            <v-card-actions> </v-card-actions>
           </v-card>
         </v-col>
       </v-row>
